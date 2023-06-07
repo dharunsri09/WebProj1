fa7541340f499e81018611d52373c2283a92b24a
DHARUN M <115633964+dharunsri09@users.noreply.github.com>
2023-06-08 00:57:34 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,17 +1,1 @@
-#!/bin/bash
-
-#build the
-
-echo "Building the Prpject ..."
-python -m pip install -r requirement.txt
-pip install django
-#Run
-
-echo "Make Migration..."
-python manage.py makemigrations --noinput
-python manage.py migrate --noinput
-
-echo "Collect .."
-python manage.py collectstatic --noinput --clear
-
 
